5fc5d1a5409e3eca60da0ac27f550f84ac60c647
dengqingyong <dengqingyong@laiye.com>
2025-07-29 16:54:18 +0800

    init: 项目名称中性化，改名为 dbox

---
+++ release_to_laiye.bat
@@ -0,0 +1,17 @@
+@echo off
+rd /q /s build
+rd /q /s dist
+rd /q /s dbox.egg-info
+echo "After the last packing record is cleared, press any key to continue packing"
+pause
+
+pip install --upgrade setuptools wheel twine
+python setup.py sdist bdist_wheel
+twine check dist/*
+echo "Packaging successful, need to publish to��pypi source��and��LaiYe source��. Press any key to continue publishing"
+pause
+
+twine upload -r laiye dist/*
+twine upload dist/*
+echo "Release success"
+pause
